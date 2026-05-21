511c1f5353e6f642618469cd09fef51c7802c234
amaan-19 <amaanakhan523@gmail.com>
2026-05-21 11:19:12 -0500

    deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import deno from '@deno/astro-adapter';
 import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: deno(),
+  site: 'https://amaankhan.info',
   integrations: [preact()]
 });

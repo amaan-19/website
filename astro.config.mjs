0edf3097e745ac28b4250be28ef1a9eef51f3b0b
amaan-19 <amaanakhan523@gmail.com>
2026-07-04 14:48:17 -0500

    added local hosting and latex rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 import { defineConfig } from 'astro/config';
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'server',
+  server: {
+    host: true
+  },
   adapter: vercel({
     webAnalytics: {
       enabled: true, 
     },
   }),
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   site: 'https://amaankhan.info',
 });
